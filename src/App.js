ccd940eec23271e1782e733dde88a42c42921ae8
Uzair Afridi <uzairafridi00@gmail.com>
2022-08-06 08:24:18 +0500

    Icons imported

--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+import {
+  IoMdSunny,
+  IoMdRainy,
+  IoMdCloudy,
+  IoMdSnow,
+  IoMdThunderstorm,
+  IoMdSearch,
+} from "react-icons/io";
+
+import {
+  BsCloudHaze2Fill,
+  BsCloudDrizzleFill,
+  BsEye,
+  BsWater,
+  BsThermometer,
+  BsWind,
+} from "react-icons/bs";
+
+import { TbTemperatureCelsius } from "react-icons/tb";
+import { ImSpinner } from "react-icons/im";
 
 const App = () => {
   return <div>react app</div>;
