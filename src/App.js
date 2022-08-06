14ec041b690a540bfae10011902a6b7514531436
Uzair Afridi <uzairafridi00@gmail.com>
2022-08-06 10:24:01 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,21 +27,29 @@ const APIKey = "121a1e9098930ed3016ffe8aa84b5752";
 
 const App = () => {
   const [data, setData] = useState(null);
-  const [location, setLocation] = useState("Peshawar");
+  const [location, setLocation] = useState("Islamabad");
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInput = (e) => {
+    setInputValue(e.target.value);
+  };
 
   useEffect(() => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIKey}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${APIKey}`;
     axios.get(url).then((res) => {
       setData(res.data);
     });
   }, [location]);
 
-  // console.log(data);
+  console.log(data);
 
   if (!data) {
     return (
       <div>
-        <div>
+        <div
+          className="flex flex-col 
+      items-center justify-center"
+        >
           <ImSpinner8 className="text-5xl animate-spin" />
         </div>
       </div>
@@ -75,20 +83,125 @@ const App = () => {
       icon = <IoMdThunderstorm />;
       break;
   }
+
+  // Date Object
+  const date = new Date();
+
   return (
     <>
-      <div className="w-full h-screen bg-gradientBg">
+      <div
+        className="w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col 
+      items-center justify-center px-4 lg:px-0"
+      >
         {/* form */}
-        <form>form</form>
+        <form
+          onChange={(e) => handleInput(e)}
+          className="h-16 bg-black/30 w-full max-w-[450px] rounded-full backdrop-blur-[32px]
+        mb-8"
+        >
+          <div className="h-full relative flex items-center justify-between p-2">
+            <input
+              type="text"
+              placeholder="Search by City or Country..."
+              className="flex-1 bg-transparent outline-none placeholder:text-white text-white text-[15px]
+              font-light pl-6 h-full"
+            />
+            <button
+              className="bg-[#1ab8ed] hover:bg-[#15abdd] w-20 h-12 rounded-full flex justify-center items-center
+            transition"
+            >
+              <IoMdSearch className="text-2xl text-white" />
+            </button>
+          </div>
+        </form>
+
         {/* card */}
-        <div>
+        <div
+          className="w-full max-w-[450px] bg-black/20 min-h-[584px] text-white backdrop-blur-[32px] 
+        rounded-[32px] py-12 px-6"
+        >
           <div>
             {/* card top */}
-            <div>card top</div>
+            <div className="flex items-center gap-x-5">
+              {/* icon */}
+              <div className="text-[87px]">{icon}</div>
+              <div>
+                {/* country name */}
+                <div className="text-2xl font-semibold">
+                  {data.name}, {data.sys.country}
+                </div>
+                {/* date */}
+                <div>
+                  {date.getUTCDate()}/{date.getUTCMonth() + 1}/
+                  {date.getUTCFullYear()}
+                </div>
+              </div>
+            </div>
             {/* card body */}
-            <div>card body</div>
+            <div className="my-20">
+              <div className="flex justify-center items-center">
+                <div className="text-[144px] leading-none font-light">
+                  {parseInt(data.main.temp)}
+                </div>
+                <div className="text-4xl">{<TbTemperatureCelsius />}</div>
+              </div>
+              <div className="capitalize text-center">
+                {data.weather[0].description}
+              </div>
+            </div>
             {/* card bottom */}
-            <div>card bottom</div>
+            <div className="max-w-[378px] mx-auto flex flex-col gap-y-6">
+              <div className="flex justify-between">
+                <div className="flex items-center gap-x-2">
+                  {/* icons */}
+                  <div className="text-[20px]">
+                    <BsEye />
+                  </div>
+                  <div>
+                    Visibility
+                    <span className="ml-2">{data.visibility / 1000} km</span>
+                  </div>
+                </div>
+
+                <div className="flex items-center gap-x-2">
+                  {/* icons */}
+                  <div className="text-[20px]">
+                    <BsThermometer />
+                  </div>
+                  <div className="flex">
+                    Feels Like
+                    <span className="flex ml-2">
+                      {parseInt(data.main.feels_like)}
+                      <TbTemperatureCelsius />
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex justify-between">
+                <div className="flex items-center gap-x-2">
+                  {/* icons */}
+                  <div className="text-[20px]">
+                    <BsWater />
+                  </div>
+                  <div>
+                    Humidity
+                    <span className="ml-2">{data.main.humidity} %</span>
+                  </div>
+                </div>
+
+                <div className="flex items-center gap-x-2">
+                  {/* icons */}
+                  <div className="text-[20px]">
+                    <BsWind />
+                  </div>
+                  <div>
+                    Wind
+                    <span className="ml-2">{data.wind.speed} m/s</span>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
