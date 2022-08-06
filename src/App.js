05584f4e774192ecb77aa6281735d3856a4aabba
Uzair Afridi <uzairafridi00@gmail.com>
2022-08-06 12:17:34 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 
+// import axios
 import axios from "axios";
+
+// import icons
 import {
   IoMdSunny,
   IoMdRainy,
@@ -22,46 +25,86 @@ import {
 import { TbTemperatureCelsius } from "react-icons/tb";
 import { ImSpinner8 } from "react-icons/im";
 
-// 121a1e9098930ed3016ffe8aa84b5752
-const APIKey = "121a1e9098930ed3016ffe8aa84b5752";
+// api key
+const APIkey = "bcf2048bc3be154bded8f277f580ba2e";
 
 const App = () => {
   const [data, setData] = useState(null);
-  const [location, setLocation] = useState("Islamabad");
+  const [location, setLocation] = useState("Peshawar");
   const [inputValue, setInputValue] = useState("");
+  const [animate, setAnimate] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
 
   const handleInput = (e) => {
     setInputValue(e.target.value);
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    // console.log(inputValue);
+    // if input value is not empty
     if (inputValue !== "") {
+      // set location
       setLocation(inputValue);
     }
-    // Select Input and clear the input value
+
+    // select input
     const input = document.querySelector("input");
+
+    // if input value is empty
+    if (input.value === "") {
+      // set animate to true
+      setAnimate(true);
+      // after 500 ms set animate to false
+      setTimeout(() => {
+        setAnimate(false);
+      }, 500);
+    }
+
+    // clear input
     input.value = "";
+
+    // prevent defaults
+    e.preventDefault();
   };
 
+  // fetch the data
   useEffect(() => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${APIKey}`;
-    axios.get(url).then((res) => {
-      setData(res.data);
-    });
+    // set loading to true
+    setLoading(true);
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${APIkey}`;
+
+    axios
+      .get(url)
+      .then((res) => {
+        // set the data after 1500 ms
+        setTimeout(() => {
+          setData(res.data);
+          // set loading to false
+          setLoading(false);
+        }, 1500);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setErrorMsg(err);
+      });
   }, [location]);
 
-  // console.log(data);
+  // error message
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setErrorMsg("");
+    }, 2000);
+    // clear timer
+    return () => clearTimeout(timer);
+  }, [errorMsg]);
 
+  // if data is false show the loader
   if (!data) {
     return (
-      <div>
-        <div
-          className="flex flex-col 
-      items-center justify-center"
-        >
-          <ImSpinner8 className="text-5xl animate-spin" />
+      <div className="w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col justify-center items-center">
+        <div>
+          <ImSpinner8 className="text-5xl animate-spin text-white" />
         </div>
       </div>
     );
@@ -69,7 +112,7 @@ const App = () => {
 
   // set the icon according to the weather
   let icon;
-  // console.log(data.weather[0].main);
+
   // eslint-disable-next-line default-case
   switch (data.weather[0].main) {
     case "Clouds":
@@ -95,43 +138,43 @@ const App = () => {
       break;
   }
 
-  // Date Object
+  // date object
   const date = new Date();
 
   return (
-    <>
-      <div
-        className="w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col 
-      items-center justify-center px-4 lg:px-0"
+    <div className="w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center px-4 lg:px-0">
+      {errorMsg && (
+        <div className="w-full max-w-[90vw] lg:max-w-[450px] bg-[#ff208c] text-white absolute top-2 lg:top-10 p-4 capitalize rounded-md">{`${errorMsg.response.data.message}`}</div>
+      )}
+      {/* form */}
+      <form
+        className={`${
+          animate ? "animate-shake" : "animate-none"
+        } h-16 bg-black/30 w-full max-w-[450px]
+      rounded-full backdrop-blur-[32px] mb-8`}
       >
-        {/* form */}
-        <form
-          onChange={(e) => handleInput(e)}
-          className="h-16 bg-black/30 w-full max-w-[450px] rounded-full backdrop-blur-[32px]
-        mb-8"
-        >
-          <div className="h-full relative flex items-center justify-between p-2">
-            <input
-              type="text"
-              placeholder="Search by City or Country..."
-              className="flex-1 bg-transparent outline-none placeholder:text-white text-white text-[15px]
-              font-light pl-6 h-full"
-            />
-            <button
-              onClick={(e) => handleSubmit(e)}
-              className="bg-[#1ab8ed] hover:bg-[#15abdd] w-20 h-12 rounded-full flex justify-center items-center
-            transition"
-            >
-              <IoMdSearch className="text-2xl text-white" />
-            </button>
+        <div className="h-full relative flex items-center justify-between p-2">
+          <input
+            onChange={(e) => handleInput(e)}
+            className="flex-1 bg-transparent outline-none placeholder:text-white text-white text-[15px] font-light pl-6 h-full"
+            type="text"
+            placeholder="Search by city or country"
+          />
+          <button
+            onClick={(e) => handleSubmit(e)}
+            className="bg-[#1ab8ed] hover:bg-[#15abdd] w-20 h-12 rounded-full flex justify-center items-center transition"
+          >
+            <IoMdSearch className="text-2xl text-white" />
+          </button>
+        </div>
+      </form>
+      {/* card */}
+      <div className="w-full max-w-[450px] bg-black/20 min-h-[584px] text-white backdrop-blur-[32px] rounded-[32px] py-12 px-6">
+        {loading ? (
+          <div className="w-full h-full flex justify-center items-center">
+            <ImSpinner8 className="text-white text-5xl animate-spin" />
           </div>
-        </form>
-
-        {/* card */}
-        <div
-          className="w-full max-w-[450px] bg-black/20 min-h-[584px] text-white backdrop-blur-[32px] 
-        rounded-[32px] py-12 px-6"
-        >
+        ) : (
           <div>
             {/* card top */}
             <div className="flex items-center gap-x-5">
@@ -152,11 +195,16 @@ const App = () => {
             {/* card body */}
             <div className="my-20">
               <div className="flex justify-center items-center">
+                {/* temp */}
                 <div className="text-[144px] leading-none font-light">
                   {parseInt(data.main.temp)}
                 </div>
-                <div className="text-4xl">{<TbTemperatureCelsius />}</div>
+                {/* celsius icon */}
+                <div className="text-4xl">
+                  <TbTemperatureCelsius />
+                </div>
               </div>
+              {/* weather description */}
               <div className="capitalize text-center">
                 {data.weather[0].description}
               </div>
@@ -165,34 +213,32 @@ const App = () => {
             <div className="max-w-[378px] mx-auto flex flex-col gap-y-6">
               <div className="flex justify-between">
                 <div className="flex items-center gap-x-2">
-                  {/* icons */}
+                  {/* icon */}
                   <div className="text-[20px]">
                     <BsEye />
                   </div>
                   <div>
-                    Visibility
+                    Visibility{" "}
                     <span className="ml-2">{data.visibility / 1000} km</span>
                   </div>
                 </div>
-
                 <div className="flex items-center gap-x-2">
-                  {/* icons */}
+                  {/* icon */}
                   <div className="text-[20px]">
                     <BsThermometer />
                   </div>
                   <div className="flex">
-                    Feels Like
-                    <span className="flex ml-2">
+                    Feels like
+                    <div className="flex ml-2">
                       {parseInt(data.main.feels_like)}
                       <TbTemperatureCelsius />
-                    </span>
+                    </div>
                   </div>
                 </div>
               </div>
-
               <div className="flex justify-between">
                 <div className="flex items-center gap-x-2">
-                  {/* icons */}
+                  {/* icon */}
                   <div className="text-[20px]">
                     <BsWater />
                   </div>
@@ -201,23 +247,21 @@ const App = () => {
                     <span className="ml-2">{data.main.humidity} %</span>
                   </div>
                 </div>
-
                 <div className="flex items-center gap-x-2">
-                  {/* icons */}
+                  {/* icon */}
                   <div className="text-[20px]">
                     <BsWind />
                   </div>
                   <div>
-                    Wind
-                    <span className="ml-2">{data.wind.speed} m/s</span>
+                    Wind <span className="ml-2">{data.wind.speed} m/s</span>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
